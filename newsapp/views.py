51d07e366c84c9b9ab554a64a864bc3d6348538a
JOD4004 <109351785+JOD4004@users.noreply.github.com>
2022-07-17 11:52:01 +0545

    Update views.py

--- newsapp/views.py
+++ newsapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from newsapi import NewsApiClient
 # Create your views here.
 def index(request):
-    newsapi=NewsApiClient(api_key='70650593a85c4d25b73ce776d54f12bd')
+    newsapi=NewsApiClient(api_key='...............')
     if request.method=='POST':
         q=request.POST['word']
         headlines=newsapi.get_top_headlines(q=q)
@@ -26,7 +26,7 @@ def index(request):
     return render(request,'index.html',{'mylist':mylist})
 
 def business(request):
-    newsapi=NewsApiClient(api_key='70650593a85c4d25b73ce776d54f12bd')
+    newsapi=NewsApiClient(api_key='.................')
     headlines=newsapi.get_top_headlines(category='business')
     articles=headlines['articles']
     desc=[]
@@ -43,7 +43,7 @@ def business(request):
     return render(request,'business.html',{'mylist':mylist})
 
 def entertainment(request):
-    newsapi=NewsApiClient(api_key='70650593a85c4d25b73ce776d54f12bd')
+    newsapi=NewsApiClient(api_key='.................')
     headlines=newsapi.get_top_headlines(category='entertainment')
     articles=headlines['articles']
     desc=[]
@@ -61,7 +61,7 @@ def entertainment(request):
     return render(request,'entertainment.html',{'mylist':mylist})
 
 def general(request):
-    newsapi=NewsApiClient(api_key='70650593a85c4d25b73ce776d54f12bd')
+    newsapi=NewsApiClient(api_key='.....................')
     headlines=newsapi.get_top_headlines(category='general')
     articles=headlines['articles']
     desc=[]
@@ -78,7 +78,7 @@ def general(request):
     return render(request,'general.html',{'mylist':mylist})
 
 def health(request):
-    newsapi=NewsApiClient(api_key='70650593a85c4d25b73ce776d54f12bd')
+    newsapi=NewsApiClient(api_key='...................')
     headlines=newsapi.get_top_headlines(category='health')
     articles=headlines['articles']
     desc=[]
@@ -96,7 +96,7 @@ def health(request):
     return render(request,'health.html',{'mylist':mylist})
 
 def science(request):
-    newsapi=NewsApiClient(api_key='70650593a85c4d25b73ce776d54f12bd')
+    newsapi=NewsApiClient(api_key='.........................')
     headlines=newsapi.get_top_headlines(category='science')
     articles=headlines['articles']
     desc=[]
@@ -113,7 +113,7 @@ def science(request):
     return render(request,'science.html',{'mylist':mylist})
 
 def sports(request):
-    newsapi=NewsApiClient(api_key='70650593a85c4d25b73ce776d54f12bd')
+    newsapi=NewsApiClient(api_key='........................')
     headlines=newsapi.get_top_headlines(category='sports')
     articles=headlines['articles']
     desc=[]
@@ -131,7 +131,7 @@ def sports(request):
     return render(request,'sports.html',{'mylist':mylist})
 
 def technology(request):
-    newsapi=NewsApiClient(api_key='70650593a85c4d25b73ce776d54f12bd')
+    newsapi=NewsApiClient(api_key='.........................')
     headlines=newsapi.get_top_headlines(category='technology')
     articles=headlines['articles']
     desc=[]
